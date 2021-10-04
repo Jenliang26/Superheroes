15aec3cd0ea989b2c0d85a10124647ca102d2c82
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-10-03 22:20:13 -0500

    Added Heroes class to models and registered it in admin.py

--- superheroesdatabase/heroes/models.py
+++ superheroesdatabase/heroes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+class Heroes (models.Model):
+    
+    super_hero_name = models.CharField(max_length=50)
+    alter_ego = models.CharField(max_length=50)
+    primary_power = models.CharField(max_length=50)
+    secondary_power = models.CharField(max_length=50)
+    catchphrase = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.super_hero_name
