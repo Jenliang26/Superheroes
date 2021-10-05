9849566d436b3d2560eee0a9b2187158e7138556
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-10-04 19:03:19 -0500

    Created single endpoint for heroes app

--- superheroesdatabase/heroes/urls.py
+++ superheroesdatabase/heroes/urls.py
@@ -1,1 +1,8 @@
+from django.urls.resolvers import URLPattern
 from . import views
+from django.urls import path
+
+app_name = 'heroes'
+urlpatterns = [
+    path('index', views.index, name='index')
+]
